74973db452c4ed892853a48c69a85c0fc4b63f6f
pazia008 <pazia008@gmail.com>
2020-11-19 13:36:04 -0600

    added javascript to sort fish by lenght

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -39,3 +39,46 @@ const fishCollection = [{
 export const useFish = () => {
     return fishCollection.slice()
 }
+
+
+
+
+
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+
+    for (const fish of fishCollection) {
+        if (fish.length % 3 === 0) {
+            holyFish.push(fish)
+        }
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiers = []
+    for (const fish of fishCollection) {
+        if (fish.length % 5 === 0 && fish.length % 3 !== 0) {
+            soldiers.push(fish)
+        }
+    }
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const regularFish = []
+
+    for (const fish of fishCollection) {
+        if (fish.length % 5 !== 0 && fish.length % 3 !== 0) {
+            regularFish.push(fish)
+        }
+    }
+
+
+    return regularFish
+}
--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,13 +1,17 @@
-import { useFish } from './FishDataProvider.js '
+import { mostHolyFish } from './FishDataProvider.js '
+import { soldierFish } from './FishDataProvider.js '
+import { nonHolyFish } from './FishDataProvider.js '
 import { Fish } from './Fish.js'
 
 export const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
     const contentElement = document.querySelector(".fishList")
-    const allTheFish = useFish()
+    const holyFish = mostHolyFish()
+    const soldiers = soldierFish()
+    const losers = nonHolyFish()
 
-    for (const fishObject of allTheFish) {
+    for (const fishObject of holyFish) {
         //console.log("one fish!", fishObject)
         const fishHTML = Fish(fishObject)
             // Add to the existing HTML in the content element
@@ -17,4 +21,24 @@ export const FishList = () => {
 
     }
 
+
+    for (const fishObject of soldiers) {
+        //console.log("one fish!", fishObject)
+        const fishHTML = Fish(fishObject)
+            // Add to the existing HTML in the content element
+        contentElement.innerHTML += fishHTML
+
+
+
+    }
+
+    for (const fishObject of losers) {
+        //console.log("one fish!", fishObject)
+        const fishHTML = Fish(fishObject)
+            // Add to the existing HTML in the content element
+        contentElement.innerHTML += fishHTML
+
+
+
+    }
 }
